c89e8cde644fcdd9bf81d57eee35187e599dbe42
acamanchaturvedi <acamanchaturvedi@gmail.com>
2024-09-12 22:17:13 +0530

    add some code for entities 2

--- src/main/java/com/notabusiness/app/entity/Worker.java
+++ src/main/java/com/notabusiness/app/entity/Worker.java
@@ -11,15 +11,11 @@ public class Worker {
     private int workerId;
     private int userId;
     private LocalDateTime timestamp;
-    private List<Integer> skillIds;
     private String bio;
-    private String aadharOrCollegeIdNumber;
     private String title;
     private String experience;
     private double totalRating;
     private int totalReviewCount;
-    private List<Integer> reviewIds;
-    private List<String> tagIds;
     private String photoId;
 
 }
