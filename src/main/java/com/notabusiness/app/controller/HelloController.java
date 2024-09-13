2c2a8ed29a116e16c8206def26cfad97b14702bb
acamanchaturvedi <acamanchaturvedi@gmail.com>
2024-09-13 23:49:08 +0530

    check db connection

--- src/main/java/com/notabusiness/app/controller/HelloController.java
+++ src/main/java/com/notabusiness/app/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.notabusiness.app.controller;
 
+import com.notabusiness.app.entity.Category;
+import com.notabusiness.app.repo.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final CategoryRepository categoryRepository;
 
     @GetMapping("/")
     public String index() {
+        Category testOneCategory = new Category();
+        testOneCategory.setName("test one");
+        categoryRepository.saveAndFlush(testOneCategory);
         return "Hello Aman!";
     }
 
