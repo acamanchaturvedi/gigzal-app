fa2e2c79de1576e1f6625283895f538b67afbe54
ro13hit <13rohitgarg@gmail.com>
2024-09-24 23:12:38 +0530

    added more entity files

--- src/main/java/com/notabusiness/app/entity/Users.java
+++ src/main/java/com/notabusiness/app/entity/Users.java
@@ -3,6 +3,8 @@ package com.notabusiness.app.entity;
 import com.notabusiness.app.util.Auditable;
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.List;
+import com.notabusiness.app.util.UserRole;
 
 @Data
 @Entity
@@ -12,4 +14,43 @@ public class Users extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private int userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "city")
+    private String city;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Review> submittedReviews;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_appointment_history",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "appointment_id")
+    )
+    private List<Appointment> appointmentHistory;
+
+    @Column(name = "photo_id")
+    private Long photoId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role")
+    private UserRole userRole;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
 }
