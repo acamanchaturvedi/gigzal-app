bba86632c0a26503a74c7ad9552aab0df3640d46
acamanchaturvedi <acamanchaturvedi@gmail.com>
2024-10-27 12:46:30 +0530

    refactor code 2

--- src/main/java/com/notabusiness/app/controller/UserController.java
+++ src/main/java/com/notabusiness/app/controller/UserController.java
@@ -6,7 +6,6 @@ import com.notabusiness.app.gigzal.generated.controller.AuthenticationApi;
 import com.notabusiness.app.gigzal.generated.model.LoginResponse;
 import com.notabusiness.app.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
